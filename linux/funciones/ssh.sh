e6d003266fb22c7dab160fd2395f22bb59f55dc2
Zai <arizazaira23@gmail.com>
2026-03-18 20:29:39 -0700

    refactor: limpiar salida del modulo SSH en Linux

--- linux/funciones/ssh.sh
+++ linux/funciones/ssh.sh
@@ -1,17 +1,18 @@
+
 #!/bin/bash
 function configurar_ssh() {
 
 echo "Actualizando paquetes...."
-apt update
+apt update > /dev/null 2>&1
 
 echo "Instalando OpenSSH Server..."
-apt install -y openssh-server
+apt install -y openssh-server > /dev/null 2>&1
 
 echo "Habilitando servicio SSH..."
-systemctl enable ssh
+systemctl enable ssh > /dev/null 2>&1
 
 echo "Iniciando servicio SSH..."
-systemctl start ssh
+systemctl start ssh > /dev/null/ 2>&1
 
 echo "Verificando estado del servicio SSH..."
 estado=$(systemctl is-active ssh)
